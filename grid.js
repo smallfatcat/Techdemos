f5f487c753566d24d40561c62abed9e137a509a3
David Imrie <david.imrie@gmail.com>
2025-02-14 19:59:10 +0000

    mostly working demo

--- grid.js
+++ grid.js
@@ -31,20 +31,16 @@ const neighbours = [
     [1, 2],
     [1, 2, 3],
     [2, 3],
-    [3,4,5],
-    [4,5],
+    [3, 4, 5],
+    [4, 5],
 ];
 
 // let nullcount = 0;
 let canvas = undefined;
 let ctx = undefined;
 
-const gSize = 50;
+const gSize = 100;
 let gCons = [];
-const U = -10;
-const D = 10;
-const L = -1;
-const R = 1;
 const neighbourOffsets = [gSize * -1, gSize, -1, 1];
 for (let i = 0; i < gSize * gSize; i++) {
     gCons.push(defaultConstraints.slice());
@@ -74,17 +70,15 @@ function removeConstraints(originals, valids) {
 function propogateConstraints(i) {
     // check each neighbour
     for (let neighbourOffset of neighbourOffsets) {
-        // console.log((i + neighbourOffset < 0), ((i + neighbourOffset) >= gSize * gSize));
         if ((i + neighbourOffset < 0) || (i + neighbourOffset >= gSize * gSize)) {
             continue;
         }
-        // console.log("here", gCons[i + neighbourOffset].length < 2);
         if (gCons[i + neighbourOffset].length < 2) {
             continue;
         }
         let newCons = removeConstraints(gCons[i + neighbourOffset], getValids(gCons[i]));
         if (newCons.length == gCons[i + neighbourOffset].length) {
-            continue;
+            return;
         }
         gCons[i + neighbourOffset] = newCons;
         propogateConstraints(i + neighbourOffset);
@@ -110,43 +104,54 @@ function collapse(i) {
 
 function test() {
     let i = getSmallest();
-    for ( let j = 0; j < 100000; j++) {
+    for (let j = 0; j < 100000; j++) {
         i = getSmallest();
         collapse(i);
         propogateConstraints(i);
     }
 }
 
+let completed = 0;
+
 function getSmallest() {
+    completed = 0;
     let lowest = Infinity;
     let lowestIndex = 0;
     for (let i in gCons) {
+        if(gCons[i].length == 1){
+            completed += 1;
+        }
         if (gCons[i].length < lowest && gCons[i].length > 1) {
             lowest = gCons[i].length;
             lowestIndex = i;
         }
     }
+    console.log("completed:" + completed);
     // console.log(lowestIndex)
     return Number(lowestIndex);
 }
 
 function animate(lastFrameTime) {
     let i = getSmallest();
-    collapse(i);
-    propogateConstraints(i);
-    draw(ctx);
-    requestAnimationFrame(animate);
+    if(completed != gSize * gSize){
+        collapse(i);
+        propogateConstraints(i);
+        draw(ctx);
+        requestAnimationFrame(animate);
+    }
 }
 
+let cellSize = 5;
+
 function draw(ctx) {
-    for (let i in gCons){
+    for (let i in gCons) {
         ctx.lineWidth = 2;
         ctx.fillStyle = gCons[i].length == 1 ? colors[gCons[i][0]] : "black";
         let coord = indexToCoord(i);
-        ctx.fillRect(coord[0] * 10, coord[1] * 10, 10, 10);
+        ctx.fillRect(coord[0] * cellSize, coord[1] * cellSize, cellSize, cellSize);
         // console.log(coord[0] * 10, coord[1] * 10, 10, 10);
     }
-    
+
     // ctx.strokeStyle = 'gray';
     // ctx.strokeRect(cell.x, cell.y, cell.width, cell.height);
     // ctx.font = "10px Arial";
@@ -157,8 +162,10 @@ function draw(ctx) {
 window.onload = (event) => {
     canvas = document.getElementById("gridCanvas");
     ctx = canvas.getContext("2d");
-    canvas.width = gSize * 10;
-    canvas.height = gSize * 10;
-    collapse((gSize*gSize)/2);
+    canvas.width = gSize * cellSize;
+    canvas.height = gSize * cellSize;
+    collapse((gSize * gSize) / 2 + gSize / 2);
+    propogateConstraints((gSize * gSize) / 2 + gSize / 2);
+    draw(ctx);
     animate();
 }

8828c40b8ddacca6e9cf83cec9586e22c8866486
David Imrie <david.imrie@gmail.com>
2025-02-14 12:55:37 +0000

    fixed

--- grid.js
+++ grid.js
@@ -1,8 +1,11 @@
 const colors = [
     "blue",
+    "blue",
     "yellow",
     "green",
+    "darkgreen",
     "brown",
+
 ];
 
 const defaultConstraints = [
@@ -10,6 +13,8 @@ const defaultConstraints = [
     1,
     2,
     3,
+    4,
+    5,
 ];
 
 // const ratios = [
@@ -23,14 +28,16 @@ const defaultConstraints = [
 
 const neighbours = [
     [0, 1],
-    [0, 1, 2],
+    [1, 2],
     [1, 2, 3],
     [2, 3],
+    [3,4,5],
+    [4,5],
 ];
 
 // let nullcount = 0;
-// let canvas = undefined;
-// let ctx = undefined;
+let canvas = undefined;
+let ctx = undefined;
 // let cells = [];
 // const gridWidth = 500;
 // const gridHeight = 500;
@@ -65,13 +72,13 @@ const neighbours = [
 //     }
 // }
 
-const gSize = 10;
+const gSize = 50;
 let gCons = [];
 const U = -10;
 const D = 10;
 const L = -1;
 const R = 1;
-const neighbourOffsets = [-10, 10, -1, 1];
+const neighbourOffsets = [gSize * -1, gSize, -1, 1];
 for (let i = 0; i < gSize * gSize; i++) {
     gCons.push(defaultConstraints.slice());
 }
@@ -89,8 +96,8 @@ function coordToIndex(coord) {
 
 function removeConstraints(originals, valids) {
     let newCons = [];
-    for (let con of originals){
-        if (valids.includes(con)){
+    for (let con of originals) {
+        if (valids.includes(con)) {
             newCons.push(con);
         }
     }
@@ -100,14 +107,16 @@ function removeConstraints(originals, valids) {
 function propogateConstraints(i) {
     // check each neighbour
     for (let neighbourOffset of neighbourOffsets) {
-        if ((i + neighbourOffset < 0) || (i + neighbourOffset >= gSize * gSize)){
+        console.log((i + neighbourOffset < 0), ((i + neighbourOffset) >= gSize * gSize));
+        if ((i + neighbourOffset < 0) || (i + neighbourOffset >= gSize * gSize)) {
             continue;
         }
-        if(gCons[i + neighbourOffset].length < 2){
-            return;
+        console.log("here", gCons[i + neighbourOffset].length < 2);
+        if (gCons[i + neighbourOffset].length < 2) {
+            continue;
         }
         let newCons = removeConstraints(gCons[i + neighbourOffset], getValids(gCons[i]));
-        if(newCons.length == gCons[i + neighbourOffset].length){
+        if (newCons.length == gCons[i + neighbourOffset].length) {
             return;
         }
         gCons[i + neighbourOffset] = newCons;
@@ -117,9 +126,9 @@ function propogateConstraints(i) {
 
 function getValids(constraints) {
     let valids = [];
-    for(let c of constraints){
-        for(let n of neighbours[c]){
-            if(!valids.includes(n)){
+    for (let c of constraints) {
+        for (let n of neighbours[c]) {
+            if (!valids.includes(n)) {
                 valids.push(n);
             }
         }
@@ -127,31 +136,82 @@ function getValids(constraints) {
     return valids;
 }
 
-function collapse(){
-    
+function collapse(i) {
+    let r = Math.floor(Math.random() * gCons[i].length)
+    gCons[i] = [gCons[i][r]];
+}
+
+function test() {
+    let i = getSmallest();
+    for ( let j = 0; j < 100000; j++) {
+        i = getSmallest();
+        collapse(i);
+        propogateConstraints(i);
+    }
+}
+
+function getSmallest() {
+    let lowest = Infinity;
+    let lowestIndex = 0;
+    for (let i in gCons) {
+        if (gCons[i].length < lowest && gCons[i].length > 1) {
+            lowest = gCons[i].length;
+            lowestIndex = i;
+        }
+    }
+    // console.log(lowestIndex)
+    return Number(lowestIndex);
+}
+
+function animate(lastFrameTime) {
+    let i = getSmallest();
+    collapse(i);
+    propogateConstraints(i);
+    draw(ctx);
+    requestAnimationFrame(animate);
 }
 
 
-// function draw(ctx, cell) {
-//     ctx.lineWidth = 2;
-//     ctx.fillStyle = cell.constraints.length == 1 ? colors[cell.constraints[0]] : "black";
-//     ctx.fillRect(cell.x, cell.y, cell.width, cell.height);
-//     // ctx.strokeStyle = 'gray';
-//     // ctx.strokeRect(cell.x, cell.y, cell.width, cell.height);
-//     // ctx.font = "10px Arial";
-//     // ctx.fillStyle = "white";
-//     // ctx.fillText(cell.constraints.length, cell.x + cellWidth / 2, cell.y + cellHeight / 2);
-// }
+function draw(ctx) {
+    for (let i in gCons){
+        ctx.lineWidth = 2;
+        ctx.fillStyle = gCons[i].length == 1 ? colors[gCons[i][0]] : "black";
+        let coord = indexToCoord(i);
+        ctx.fillRect(coord[0] * 10, coord[1] * 10, 10, 10);
+        // console.log(coord[0] * 10, coord[1] * 10, 10, 10);
+    }
+    
+    // ctx.strokeStyle = 'gray';
+    // ctx.strokeRect(cell.x, cell.y, cell.width, cell.height);
+    // ctx.font = "10px Arial";
+    // ctx.fillStyle = "white";
+    // ctx.fillText(cell.constraints.length, cell.x + cellWidth / 2, cell.y + cellHeight / 2);
+}
+
+window.onload = (event) => {
+    canvas = document.getElementById("gridCanvas");
+    ctx = canvas.getContext("2d");
+    canvas.width = gSize * 10;
+    canvas.height = gSize * 10;
+    collapse((gSize*gSize)/2);
+    animate();
+
+    // document.addEventListener('mousedown', function (event) {
+    //     let x = Math.floor(event.offsetX / cellWidth);
+    //     let y = Math.floor(event.offsetY / cellHeight);
+    //     targetCell();
+    // });
+}
 
 // function applyConstraints(constraints, cell) {
-//     if (cell.constraints.length > 0) {
-//         let constraintChanged = false;
-//         let combinedConstraints = new Set();
-//         for (let c of constraints) {
-//             for (let type of neighbours[c]) {
-//                 combinedConstraints.add(type);
-//             }
-//         }
+    //     if (cell.constraints.length > 0) {
+        //         let constraintChanged = false;
+        //         let combinedConstraints = new Set();
+        //         for (let c of constraints) {
+            //             for (let type of neighbours[c]) {
+                //                 combinedConstraints.add(type);
+                //             }
+                //         }
 
 //         for (let constraint of cell.constraints) {
 //             if (!combinedConstraints.has(constraint)) {
@@ -210,20 +270,6 @@ function collapse(){
 //     cells.push(rows);
 // }
 
-// window.onload = (event) => {
-//     canvas = document.getElementById("gridCanvas");
-//     ctx = canvas.getContext("2d");
-//     canvas.width = gridWidth;
-//     canvas.height = gridHeight;
-
-//     animate();
-
-//     document.addEventListener('mousedown', function (event) {
-//         let x = Math.floor(event.offsetX / cellWidth);
-//         let y = Math.floor(event.offsetY / cellHeight);
-//         targetCell();
-//     });
-// }
 
 // function targetCell() {
 //     oldCells = JSON.stringify(cells);
@@ -300,14 +346,4 @@ function collapse(){
 // const frameTime = 100;
 // let waitUntil = frameTime;
 
-// function animate(lastFrameTime) {
-//     previousFrameTime = lastFrameTime;
-//     for (let y = 0; y < cells.length; y++) {
-//         for (let x = 0; x < cells[0].length; x++) {
-//             draw(ctx, cells[x][y]);
-//         }
-//     }
 
-//     requestAnimationFrame(animate);
-//     targetCell();
-// }
